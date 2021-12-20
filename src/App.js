6440f00e0dbb71e4cd3d7a80ae34462679732190
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2021-12-20 08:56:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, {useState} from 'react'
-import Counter from "./components/Counter";
 import './styles/App.css';
 import SearchBar from "./components/SearchBar";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
-import CustomInput from "./components/UI/Input/CustomInput";
-import CustomButton from "./components/UI/Button/CustomButton";
 
 function App() {
 
