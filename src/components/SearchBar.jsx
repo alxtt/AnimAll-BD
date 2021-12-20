93df4e42784b4940199c629de5f7c5c384105f42
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2021-12-20 09:20:05 +0300

    try

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CustomInput from "./UI/Input/CustomInput";
+import CustomButton from "./UI/Button/CustomButton";
+import SearchButton from "./UI/Button/SearchButton";
 
 const SearchBar = (props) => {
     return (
@@ -8,6 +10,7 @@ const SearchBar = (props) => {
             <div className="search_buttons">
                 <div className="group-1">
                     <div className="rectangle">
+                        <SearchButton/>
                     </div>
                 </div>
             </div>
---
+++ src/components/UI/Button/SearchButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './SearchButton.module.css';
+
+const SearchButton = () => {
+    return (
+        <div>
+            <a href="" target="_blank" className="SearchBtn">Search</a>
+        </div>
+    );
+};
+
+export default SearchButton;
